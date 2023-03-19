8ff53af111045a69d35bb31c9c2aaadbd3c56836
Revan-Darth <nitro.Knight3080@gmail.com>
2023-03-19 20:10:21 +0700

    Added New Page & Apadtive. Add footer for main page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// $(document).ready(function(){
-	
-// });
-
 // Burger
 	const menuBurger = document.querySelector('.menu__burger');
 	const menuBody = document.querySelector('.header__nav');
@@ -12,7 +8,24 @@
 			menuBody.classList.toggle('active');
 		});
 	}
-
+// Filter
+	const filterBtnOpen = document.querySelector('.catalog__filter-btn--open');
+	const filterBtnClose = document.querySelector('.catalog__filter-btn--close');
+	const filterMenu = document.querySelector('.catalog__sidebar');
+	if(filterBtnOpen) {
+		filterBtnOpen.addEventListener('click', function (e) {
+			document.body.classList.add('lock');
+			filterBtnOpen.classList.add('_active');
+			filterMenu.classList.add('_active');
+		});
+	}
+	if(filterBtnClose) {
+		filterBtnClose.addEventListener('click', function (e) {
+			document.body.classList.remove('lock');
+			filterBtnClose.classList.remove('_active');
+			filterMenu.classList.remove('_active');
+		});
+	}
 
 // Tabs
 	const tabsBtn = document.querySelectorAll('.photos-tabs__nav-btn');
@@ -234,7 +247,7 @@
 	if (rangeSlider) {
 		noUiSlider.create(rangeSlider, {
 		    start: [4765200, 8871500],
-		    step: 25000,
+		    step: 25155,
 		    connect: true,
 		    range: {
 		        'min': [4765200],
@@ -265,4 +278,194 @@
 			});
 		});
 	}
+
+// Phone Mask
+	document.addEventListener('DOMContentLoaded', function () {
+		var phoneInputs = document.querySelectorAll('input[data-tel-input]');
+
+		var getInputNumbersValue = function (input) {
+			// Return stripped input value — just numbers
+			return input.value.replace(/\D/g, '');
+		}
+
+		var onPhonePaste = function (e) {
+			var input = e.target,
+				inputNumbersValue = getInputNumbersValue(input);
+			var pasted = e.clipboardData || window.clipboardData;
+
+			if (pasted) {
+				var pastedText = pasted.getData('Text');
+				if (/\D/g.test(pastedText)) {
+					// Attempt to paste non-numeric symbol — remove all non-numeric symbols,
+	            	// formatting will be in onPhoneInput handler
+	            	input.value = inputNumbersValue;
+	            	return;
+	            }
+			}
+		}
+
+		var onPhoneInput = function (e) {
+			var input = e.target,
+			inputNumbersValue = getInputNumbersValue(input),
+			selectionStart = input.selectionStart,
+			formattedInputValue = "";
+
+			if (!inputNumbersValue) {
+				return input.value = '';
+			}
+
+			if (input.value.length != selectionStart) {
+				// Editing in the middle of input, not last symbol
+				if (e.data && /\D/g.test(e.data)) {
+					// Attempt to input non-numeric symbol
+					input.value = inputNumbersValue;
+				}
+				return;
+			}
+			if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+				if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+				var firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+				formattedInputValue = input.value = firstSymbols + " ";
+				if (inputNumbersValue.length > 1) {
+					formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+				}
+				if (inputNumbersValue.length >= 5) {
+					formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+				}
+				if (inputNumbersValue.length >= 8) {
+					formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+				}
+				if (inputNumbersValue.length >= 10) {
+					formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+				}
+			} else {
+				formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+			}
+			input.value = formattedInputValue;
+		}
+		var onPhoneKeyDown = function (e) {
+			// Clear input after remove last symbol
+			var inputValue = e.target.value.replace(/\D/g, '');
+			if (e.keyCode == 8 && inputValue.length == 1) {
+				e.target.value = '';
+			}
+		}
+		for (var phoneInput of phoneInputs) {
+				 phoneInput.addEventListener('keydown', onPhoneKeyDown);
+				 phoneInput.addEventListener('input', onPhoneInput, false);
+				 phoneInput.addEventListener('paste', onPhonePaste, false);
+		}
+	});
+
+// Popup 
+	const popupLinks = document.querySelectorAll('.popup-link');
+	const body = document.querySelector('body');
+	const lockPadding = document.querySelectorAll('.lock-padding'); // Добавлять элементу, который багается при Lock'е
+
+	let unlock = true;
+	const timeout = 200; // Время выполнения transition, как и в CSS
+
+	if (popupLinks.length > 0) {
+		for (let index = 0; index < popupLinks.length; index++){
+			const popupLink = popupLinks[index];
+			popupLink.addEventListener('click', function (e){
+				const popupName = popupLink.getAttribute('href').replace('#', '');
+				const curentPopup = document.getElementById(popupName);
+				popupOpen(curentPopup);
+				e.preventDefault();
+			});
+		}
+	}
+	const popupCloseIcon = document.querySelectorAll('.popup__close');
+	if (popupCloseIcon.length > 0){
+		for (let index = 0; index < popupCloseIcon.length; index++){
+			const el = popupCloseIcon[index];
+			el.addEventListener('click', function (e) {
+				popupClose(el.closest('.popup'));
+				e.preventDefault();
+			});
+		}
+	}
+	function popupOpen(curentPopup) {
+		if (curentPopup && unlock){
+			const popupActive = document.querySelector('.popup.open');
+			if (popupActive) {
+				popupClose(popupActive, false);
+			} else {
+				bodyLock();
+			}
+			curentPopup.classList.add('open');
+			curentPopup.addEventListener('click', function (e) {
+				if (!e.target.closest('.popup__content')) {
+					popupClose(e.target.closest('.popup'));
+				}
+			});
+		}
+	}
+	function popupClose(popupActive, doUnlock = true) {
+		if (unlock) {
+			popupActive.classList.remove('open');
+			if (doUnlock){
+				bodyUnLock();
+			}
+		}
+	}
+	function bodyLock() {
+		const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
+		if (lockPadding.length > 0){
+			for (let index = 0; index < lockPadding.length; index++) {
+				const el = lockPadding[index];
+				el.style.paddingRight = lockPaddingValue;
+			}
+		}
+		body.style.paddingRight = lockPaddingValue;
+		body.classList.add('lock');
+		unlock = false;
+		setTimeout(function () {
+			unlock = true;
+		}, timeout);
+	}
+	function bodyUnLock() {
+		setTimeout(function () {
+			if (lockPadding.length > 0) {
+				for (let index = 0; index < lockPadding.length; index++) {
+					const el = lockPadding[index];
+					el.style.paddingRight = '0px';
+				}
+			}
+			body.style.paddingRight = '0px';
+			body.classList.remove('lock');
+		}, timeout);
+		unlock = false;
+		setTimeout(function () {
+			unlock = true;
+		}, timeout);
+	}
+	document.addEventListener('keydown', function (e) {
+		if (e.which === 27) {
+			const popupActive = document.querySelector('.popup.open');
+			popupClose(popupActive);
+		}
+	});
+
+	(function () {
+		if (!Element.prototype.closest) {
+			Element.prototype.closest = function (css) {
+				var node = this;
+				while (node) {
+					if (node.matches(css)) return node;
+					else node = node.parentElement;
+				}
+				return null;
+			};
+		}
+	})();
+	(function () {
+		if (!Element.prototype.matches) {
+			Element.prototype.matches = Element.prototype.matchesSelector ||
+				Element.prototype.webkitMatchesSelector ||
+				Element.prototype.mozMatchesSelector ||
+				Element.prototype.msMatchesSelector;
+		}
+	})();
 
